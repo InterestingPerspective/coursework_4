e47fe02e48d48a107ab72a3939bc4d566f3794d9
Lev Polevshchikov <upmindset.28@gmail.com>
2023-07-24 19:08:45 +0300

    add json_saver.py

--- utils/json_saver.py
+++ utils/json_saver.py
@@ -9,7 +9,7 @@ class WorkWithVacancies(ABC):
         pass
 
     @abstractmethod
-    def get_vacancies_by_salary(self, salary):
+    def get_vacancies_by_salary(self, salary_from, salary_to, currency=None):
         pass
 
     @abstractmethod
@@ -22,7 +22,12 @@ class JSONSaver(WorkWithVacancies):
     SAVER_FILE = "vacancies.json"
 
     def add_vacancy(self, vacancy):
-        data = {"name": vacancy.name, "url": vacancy.url, "salary": vacancy.salary, "description": vacancy.description}
+        data = {"name": vacancy.name,
+                "url": vacancy.url,
+                "salary_from": vacancy.salary_from,
+                "salary_to": vacancy.salary_to,
+                "currency": vacancy.currency,
+                "description": vacancy.description}
         if os.path.exists(self.SAVER_FILE):
             with open(self.SAVER_FILE, encoding="utf-8") as f:
                 current_data = json.load(f)
@@ -33,22 +38,18 @@ class JSONSaver(WorkWithVacancies):
             with open(self.SAVER_FILE, 'w', encoding="utf-8") as f:
                 json.dump([data], f, ensure_ascii=False, indent=4)
 
-    def get_vacancies_by_salary(self, salary):
-        suitable_vacancies = []
-        if "-" in salary:
-            frm, to = salary.replace(" ", "").strip("руб.eurEURsdUSDРУБ").split("-")
-            with open(self.SAVER_FILE, encoding="utf-8") as f:
-                current_data = json.load(f)
-            for data in current_data:
-                if int(frm) <= data["salary"] <= int(to):
-                    suitable_vacancies.append(data)
+    def get_vacancies_by_salary(self, salary_from, salary_to, currency=None):
+        if currency is None:
+            currencies = ["rub", "RUR"]
         else:
-            slr = int(salary.replace(" ", "").strip("руб.eurEURsdUSDРУБ"))
-            with open(self.SAVER_FILE, encoding="utf-8") as f:
-                current_data = json.load(f)
-            for data in current_data:
-                if data["salary"] == slr:
-                    suitable_vacancies.append(data)
+            currencies = [currency]
+        suitable_vacancies = []
+
+        with open(self.SAVER_FILE, encoding="utf-8") as f:
+            current_data = json.load(f)
+        for data in current_data:
+            if salary_from <= data["salary_from"] <= salary_to and data["currency"] in currencies:
+                suitable_vacancies.append(data)
 
         return suitable_vacancies
 
@@ -58,5 +59,6 @@ class JSONSaver(WorkWithVacancies):
         for data in current_data:
             if data["url"] == vacancy.url:
                 current_data.remove(data)
+
         with open(self.SAVER_FILE, 'w', encoding="utf-8") as f:
             json.dump(current_data, f, ensure_ascii=False, indent=4)
